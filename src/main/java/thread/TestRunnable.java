6ff22b75690313b1659f4b1510f6958ac95d09d3
junyangwei <799004508@qq.com>
2021-09-10 17:26:32 +0800

    feat: 并发 —— 创建一个线程另一种方式

--- src/main/java/thread/TestRunnable.java
+++ src/main/java/thread/TestRunnable.java
@@ -19,5 +19,25 @@ public class TestRunnable {
         };
         Thread t1 = new Thread(r);
         t1.start();
+
+        // 创建线程的另一个方式
+        MyThreadTest t2 = new MyThreadTest();
+        t2.start();
+        // 需要额外注意，调用run方法只会执行同一个线程中的任务，而不会启动新线程
+        t2.run();
+    }
+}
+
+class MyThreadTest extends Thread {
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("线程打印2：" + i);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
